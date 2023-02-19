2bf5628b8411cd86d71bcaeecba3d8337e9ecb16
Dm1triyKapshuk <125385095+Dm1triyKapshuk@users.noreply.github.com>
2023-02-19 21:44:47 +0300

    Add files via upload
    Домашка №2

---
+++ hw-2/script.js
@@ -0,0 +1,70 @@
+//Задание 1
+ let a = 20;
+
+ alert (a);
+//Задание 2
+ let release = ('год выпуска первого iPhone');
+
+ alert (release);
+//Задание 3
+ let namecreator = ('Создатель языка JavaScript');
+
+ alert (namecreator);
+//Задание 4
+ let b = 10;
+ let c = 2;
+
+ alert (b + c);
+ 
+ alert (b - c);
+ 
+ alert (b * c);
+
+ alert (b / c);
+//Задание 5
+ let result = 2**5;
+
+ alert (result);
+//Задание 6
+ let x = 9;
+ let y = 2;
+ let remains = x / y;
+
+ alert (remains);
+//Задание 7
+ let num = 1;
+ num+=5
+ num-=3
+ num*=7;
+ num/=3;
+ num+=1;
+ num-=1;
+alert(num);
+//Задание 8
+let age = prompt('Сколько вам лет?')
+
+alert (age)
+//Задание 9.0
+const user = {
+name: 'Dima',
+age: 19,
+IsAdmin:false,
+}
+// Задание 9.1
+ user.cityofresidence = 'Volgograd';
+
+//Задание 9.2
+user['age'] = 'surname';
+
+//Задание 9.3
+delete user.cityOfResidence;
+//Задание 9.4
+let info = prompt("Какую информацию хотите узнать о пользователе?");
+alert(user[info]) 
+
+//Задание 10
+
+let nam = prompt('Как вас зовут?');
+alert(`Привет, ${nam}!`)
+
+
